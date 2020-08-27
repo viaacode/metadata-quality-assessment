7a3bf65eee1c10d4c9b886b4360b9a456da5a95c
Miel Vander Sande <miel.vandersande@meemoo.be>
2020-08-27 11:32:59 +0200

    First files

---
+++ src/main/java/be/meemoo/App.java
@@ -0,0 +1,55 @@
+package be.meemoo;
+
+import com.jayway.jsonpath.InvalidJsonException;
+import de.gwdg.metadataqa.api.calculator.CalculatorFacade;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+
+        CalculatorFacade calculator = new CalculatorFacade();
+        MeemooSchema schema = new MeemooSchema();
+        calculator.setSchema(schema);
+
+        // do some configuration with the accessor of calculator Facade
+        calculator.configure();
+
+        try {
+            // initialize lines stream
+            final Path path = Paths.get("./head-json.txt");
+
+            Stream<String> stream = Files.lines(path);
+
+            // read lines
+            stream.forEach((jsonRecord) -> {
+                try {
+                    String csv = calculator.measure(jsonRecord);
+
+                    System.out.println(csv);
+                    // save csv
+                } catch (InvalidJsonException e) {
+                    // handle exception
+                }
+            });
+
+            // close the stream
+            stream.close();
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+
+}
+
+
---
+++ src/main/java/be/meemoo/MeemooSchema.java
@@ -0,0 +1,83 @@
+package be.meemoo;
+
+import de.gwdg.metadataqa.api.json.FieldGroup;
+import de.gwdg.metadataqa.api.json.JsonBranch;
+import de.gwdg.metadataqa.api.schema.Format;
+import de.gwdg.metadataqa.api.schema.Schema;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MeemooSchema implements Schema {
+
+    private static final List<FieldGroup> FIELD_GROUPS = new ArrayList<FieldGroup>();
+    private static final List<String> NO_LANGUAGE_FIELDS = new ArrayList<String>();
+    private static final Map<String, String> SOLR_FIELDS = new LinkedHashMap<String, String>();
+    private Map<String, String> extractableFields = new LinkedHashMap<String, String>();
+    private static final List<String> EMPTY_STRINGS = new ArrayList<String>();
+    private static final Map<String, JsonBranch> PATHS = new LinkedHashMap<String, JsonBranch>();
+    private static final Map<String, JsonBranch> COLLECTION_PATHS = new LinkedHashMap<String, JsonBranch>();
+
+    static {
+
+
+    }
+
+
+    public Format getFormat() {
+        return Format.XML;
+    }
+
+    public List<FieldGroup> getFieldGroups() {
+        return FIELD_GROUPS;
+    }
+
+    public List<String> getNoLanguageFields() {
+        return NO_LANGUAGE_FIELDS;
+    }
+
+    public Map<String, String> getSolrFields() {
+        return SOLR_FIELDS;
+    }
+
+    public Map<String, String> getExtractableFields() {
+        return extractableFields;
+    }
+
+    public void setExtractableFields(Map<String, String> extractableFields) {
+        this.extractableFields = extractableFields;
+    }
+
+    public List<String> getEmptyStringPaths() {
+        return EMPTY_STRINGS;
+    }
+
+    public void addExtractableField(String label, String jsonPath) {
+        extractableFields.put(label, jsonPath);
+    }
+
+    public List<JsonBranch> getCollectionPaths() {
+        return new ArrayList(COLLECTION_PATHS.values());
+    }
+
+    private static void addPath(JsonBranch branch) {
+        PATHS.put(branch.getLabel(), branch);
+        if (branch.isCollection()) {
+            COLLECTION_PATHS.put(branch.getLabel(), branch);
+        }
+    }
+
+    public List<JsonBranch> getPaths() {
+        return new ArrayList(PATHS.values());
+    }
+
+    public List<JsonBranch> getRootChildrenPaths() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public JsonBranch getPathByLabel(String label) {
+        return PATHS.get(label);
+    }
+}
